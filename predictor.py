7e3e2fcbe9a9c0b45e01c1b99d47bfab99ab390e
Sam Harreschou <sharreschou@misorobotics.com>
2024-02-14 00:30:08 -0800

    got tensorflow model running

--- predictor.py
+++ predictor.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-
+import time
 
 class Predictor:
     def __init__(self, model_path: str, expected_columns: list[str]) -> None:
@@ -15,16 +15,24 @@ class Predictor:
         serving_default = model.signatures['serving_default']
         return serving_default
     
+    
     def make_pred(self, input_data: pd.DataFrame) -> bool:
-        # if isinstance(input_data, pd.DataFrame):
-        #     # Reorder DataFrame columns to match the expected order
-        #     input_data = input_data[self.expected_columns]
+        start_time = time.time()
+        if isinstance(input_data, pd.DataFrame):
+            # Reorder DataFrame columns to match the expected order
+            input_data = input_data[self.expected_columns]
             
-        #     # Convert DataFrame to numpy array
-        #     input_data = input_data.to_numpy().astype(np.float32)
+            # Convert DataFrame to numpy array
+            input_data = input_data.to_numpy().astype(np.float32)
         
-        # input_dict = {'dense_input': tf.constant(input_data)}
-        # prediction = self.serving_default(**input_dict)
+        input_dict = {'dense_input': tf.constant(input_data)}
+        prediction = self.serving_default(**input_dict)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"elapsed_time: {elapsed_time}")
         # return prediction
-        return "mock result"
-            
+        # return "mock result"
+        # probability = prediction['dense_5'].numpy().item()
+        # return probability
+        for key, value in prediction.items():
+            print(f"Key: {key}, Value: {value.numpy()}")
